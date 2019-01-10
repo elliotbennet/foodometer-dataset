cf09ba6a1958eb4fc67332b7c608cdaad09f5939
Elliot Bennet <elliotbennet6@gmail.com>
2019-01-10 22:32:47 +0100

    Added {}/

--- compile_images_to_train.py
+++ compile_images_to_train.py
@@ -4,11 +4,11 @@ import os
 import shutil
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-training_classes = os.listdir('{}datasets'.format(current_dir))
+training_classes = os.listdir('{}/datasets'.format(current_dir))
 destination = 'train'
 
 for directory in training_classes:
-	class_dir = '{0}datasets/{1}'.format(current_dir, directory)
+	class_dir = '{0}/datasets/{1}'.format(current_dir, directory)
 	for image in os.listdir(class_dir):
 		file_name = os.path.join(class_dir, image)
 		if (os.path.isfile(file_name)):
